48bcfe723259d0742e460e7f08e7660db5675348
Johan Reza Fauzi <johanreza58@gmail.com>
2021-12-13 23:54:44 +0700

    menghapus form tambah pada data cetak kategori

--- admin/views/data-cetak-kategori.php
+++ admin/views/data-cetak-kategori.php
@@ -139,36 +139,6 @@
               <div class="card">
                 <!-- /.card-header -->
                 <div class="card-body">
-
-                  <form action="../models/m_kategori.php" method="POST">
-                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
-                      aria-labelledby="exampleModalLabel" aria-hidden="true">
-                      <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                          <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Tambah Kategori</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                              <span aria-hidden="true">&times;</span>
-                            </button>
-                          </div>
-                          <div class="modal-body">
-
-                            <div class="form-group">
-                              <label>Nama Kategori</label>
-                              <input type="text" name="nama_kategori" required="required" class="form-control"
-                                placeholder="Nama Kategori...">
-                            </div>
-
-                          </div>
-                          <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-                            <button type="submit" class="btn btn-primary">Simpan</button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </form>
-
                   <table id="example1" class="table table-bordered table-striped">
                     <thead>
                       <tr>
