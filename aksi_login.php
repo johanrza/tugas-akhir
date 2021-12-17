d6f0972ee2b1e5adb0dbacaa9cfcd24d04b43cf7
Johan Reza Fauzi <johanreza58@gmail.com>
2021-12-17 22:20:39 +0700

    Mengubah menjadi 2 level saja
    administrator & pegawai, mengubah kode error

--- aksi_login.php
+++ aksi_login.php
@@ -10,7 +10,7 @@ if(isset($_POST['login'])){
 	$result = mysqli_query($kon, "SELECT * FROM tb_login WHERE username='$username' AND password='$password'");
 
 	if (mysqli_num_rows($result)==0) {
-		$error = "Username atau Password Salah";
+		$error = "Username, Password, atau Level Salah";
 	} else {
 		$row = mysqli_fetch_assoc($result);
 		$_SESSION['id'] = $row['id'];
@@ -20,10 +20,8 @@ if(isset($_POST['login'])){
 
 		if($row['level'] == "administrator" && $level==1){
             header("Location: admin/");
-        }else if ($row['level'] == "manajemen" && $level==2) {
-            header("Location: manajemen/");
-        }else if ($row['level'] == "pribadi" && $level==3) {
-            header("Location: pribadi/");
+        }else if ($row['level'] == "pegawai" && $level==2) {
+            header("Location: pegawai/");
         }else{
             $error = "Login gagal";
         }
