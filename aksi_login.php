50bc406281b1d675db3f44b6d27bc3f76e9b04fd
Johan Reza Fauzi <johanreza58@gmail.com>
2021-12-17 16:10:10 +0700

    mengubah aksi_login (belum fix)
    Menambahkan level yaitu pribadi dan mengubah sedikit beberapa kode dari sebelumnya

--- aksi_login.php
+++ aksi_login.php
@@ -1,32 +1,30 @@
-<?php 
-$username = $_POST['username'];
-$password = md5($_POST['password']);
+<?php
 
 include 'connection.php';
 
-$user = mysqli_query($kon, "SELECT * FROM tb_login WHERE username='$username' AND password='$password'");
-$cek = mysqli_num_rows($user);
+if(isset($_POST['login'])){
+	$username = $_POST['username'];
+	$password = md5($_POST['password']);
+	$lvl = $_POST['level'];
 
-if($cek > 0){
-	session_start();
-	$row = mysqli_fetch_array($user);
-	$_SESSION['id'] = $row['id'];
-	$_SESSION['nama'] = $row['nama'];
-	$_SESSION['username'] = $row['username'];
-	$_SESSION['level'] = $row['level'];
+	$result = mysqli_query($kon, "SELECT * FROM tb_login WHERE username='$username' AND password='$password'");
 
-	if($row['level'] == "administrator"){
-		$_SESSION['status'] = "administrator_logedin";
-		header("location:admin/");
-	}else if($row['level'] == "manajemen"){
-		$_SESSION['status'] = "manajemen_logedin";
-		header("location:manajemen/");
-	}else if($row['level'] == "pribadi"){
-		$_SESSION['status'] = "pribadi_logedin";
-		header("location:pribadi/");
-	}else{
-		header("location:index.php?alert=gagal");
+	if (mysqli_num_rows($result)==0) {
+		$error = "Username atau Password Salah";
+	} else {
+		$row = mysqli_fetch_assoc($result);
+		$_SESSION['id'] = $row['id'];
+		$_SESSION['nama'] = $row['nama'];
+		$_SESSION['username'] = $row['username'];
+		$_SESSION['level'] = $row['level'];
+
+		if($row['level'] == "administrator" && $level==1){
+            header("Location: admin/");
+        }else if ($row['level'] == "Pegawai" && $level==2) {
+            header("Location: halpegawai.php");
+        }else{
+            $error = "login gagal";
+        }
 	}
-}else{
-	header("location:index.php?alert=gagal");
 }
+?>
