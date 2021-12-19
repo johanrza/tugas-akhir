baee36f940b10fc87e99cfa4b4274f4fa3299902
Johan Reza Fauzi <johanreza58@gmail.com>
2021-12-19 11:58:51 +0700

    Menambahkan include cekadmin di views folder admin
    agar nanti setelah logout tidak bisa di klik kembali (back) dan menghapus file header-table di folder admin dan pegawai

--- admin/views/data-cetak-piutang.php
+++ admin/views/data-cetak-piutang.php
@@ -1,3 +1,4 @@
+<?php include '../../cekadmin.php';?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -34,7 +35,7 @@
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="../index3.html" class="brand-link">
+      <a href="../index.php" class="brand-link">
         <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
           style="opacity: 0.8" />
         <span class="brand-text font-weight-light"><b>Pecinta</b>Uang</span><br>
